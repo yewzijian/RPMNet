b1c9ee0290a4f1f6515b22d496f8e1768c661c91
Zi Jian Yew <yewzijian@gmail.com>
2022-08-09 17:58:03 +0800

    Switch back to shapenet hosting, ignore certificate checks.

--- src/data_loader/datasets.py
+++ src/data_loader/datasets.py
@@ -224,9 +224,9 @@ class ModelNetHdf(Dataset):
     def _download_dataset(dataset_path: str):
         os.makedirs(dataset_path, exist_ok=True)
 
-        www = 'https://rpmnet.s3.us-east-2.amazonaws.com/modelnet40_ply_hdf5_2048.zip'
+        www = 'https://shapenet.cs.stanford.edu/media/modelnet40_ply_hdf5_2048.zip'
         zipfile = os.path.basename(www)
-        os.system('wget {}'.format(www))
+        os.system('wget --no-check-certificate {}'.format(www))
         os.system('unzip {} -d .'.format(zipfile))
         os.system('mv {} {}'.format(zipfile[:-4], os.path.dirname(dataset_path)))
         os.system('rm {}'.format(zipfile))
